f6eacec297bbb992adc988197db8a1f7bc50ab7f
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2026-05-06 10:05:40 +0200

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+
+def atr(df, period=14):
+    high_low = df['high'] - df['low']
+    high_close = np.abs(df['high'] - df['close'].shift())
+    low_close = np.abs(df['low'] - df['close'].shift())
+    tr = pd.concat([high_low, high_close, low_close], axis=1).max(axis=1)
+    return tr.rolling(period).mean()
